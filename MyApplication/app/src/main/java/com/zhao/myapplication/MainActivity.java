76d36b723cdd511e3cec24e30e03c2eea55b1737
Double778 <596638119@qq.com>
2016-05-09 10:52:38 +0800

    大大

--- MyApplication/app/src/main/java/com/zhao/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/zhao/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 竟无语凝噎
+        // 啊啊啊
     }
 }
